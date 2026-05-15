2a002ad1356b30e40bd3445501b466c3a904eb08
Muhsin Swabri <swabri2004@gmail.com>
2026-05-15 10:52:56 +0530

    render deployment fix

--- backend/main.py
+++ backend/main.py
@@ -495,6 +495,8 @@ def api_attendance_stop():
 # RUN SERVER
 # ==============================
 
-if __name__ == '__main__':
+import os
 
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
